6d0b2a72ef559f9085e77d21ff29e904ae4f1421
Ruslan Lyubimov <ruslan_lyubimov@epam.com>
2023-10-24 23:58:45 +0500

    added cors

--- product-service/handlers/getProductsById.mjs
+++ product-service/handlers/getProductsById.mjs
@@ -7,12 +7,20 @@ const getProductsById = async (event) => {
   if (!findProduct) {
     return {
       statusCode: 404,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+      },
       body: JSON.stringify({ message: "Product not found" }),
     };
   }
 
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
     body: JSON.stringify(findProduct),
   };
 };
--- product-service/handlers/getProductsList.mjs
+++ product-service/handlers/getProductsList.mjs
@@ -3,6 +3,10 @@ import products from "../mock-data/mockData.mjs";
 const getProductsList = async (event) => {
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
     body: JSON.stringify(products),
   };
 };
